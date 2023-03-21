56a2bf8670a26ac58e32bca5000f69da41dfb810
kevinmid3 <kevinmid3@gmail.com>
2023-03-20 19:49:57 -0700

    initial commit

--- 0x18-dynamic_libraries/3-islower.c
+++ 0x18-dynamic_libraries/3-islower.c
@@ -1,14 +1,20 @@
 #include "main.h"
 
 /**
- * _islower - checks if a character is lowercase
- * @c: the character
- * Return: 1 if letter is lowercase, 0 if not
+ *_islower - checks for lowercase character
+ *@c: character to check
+ *Return: 1 if lowercase, 0 if uppercase
  */
+
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
+	_putchar('\n');
 }
